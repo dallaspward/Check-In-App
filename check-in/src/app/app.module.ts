c33ee09c14a9206c2d88e3125cc37e8ca9d4f803
Dallas Ward <jetlock@gmail.com>
2020-04-01 14:02:01 -0400

    Add dependecies for @angular/material

--- check-in/src/app/app.module.ts
+++ check-in/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CheckInGridComponent } from './check-in-grid/check-in-grid.component';
 import { CheckInPopupComponent } from './check-in-popup/check-in-popup.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { CheckInPopupComponent } from './check-in-popup/check-in-popup.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
