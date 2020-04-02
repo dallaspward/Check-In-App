f074cfea678cdeb7fbb531351568270d54a90520
Dallas Ward <jetlock@gmail.com>
2020-04-02 12:38:15 -0400

    Add field to seed data

--- check-in/src/app/check-in-grid/check-in-grid.component.ts
+++ check-in/src/app/check-in-grid/check-in-grid.component.ts
@@ -9,13 +9,13 @@ import { Volunteer } from '../volunteer/volunteer';
 export class CheckInGridComponent implements OnInit {
 
   volunteer: Volunteer[] = [
-    {id: 1, name: "Mario"},
-    {id: 2, name: "Luigi"},
-    {id: 3, name: "Peach"},
-    {id: 4, name: "Daisy"},
-    {id: 5, name: "Wario"},
-    {id: 6, name: "Waluigi"},
-    {id: 7, name: "David Ward"}
+    {id: 1, name: "Mario", isCheckedIn: false},
+    {id: 2, name: "Luigi", isCheckedIn: false},
+    {id: 3, name: "Peach", isCheckedIn: false},
+    {id: 4, name: "Daisy", isCheckedIn: false},
+    {id: 5, name: "Wario", isCheckedIn: false},
+    {id: 6, name: "Waluigi", isCheckedIn: false},
+    {id: 7, name: "David Ward", isCheckedIn: false}
   ];
 
   constructor() { }
