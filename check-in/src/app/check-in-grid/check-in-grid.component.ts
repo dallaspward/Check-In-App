f81c45623e85a385718b2a3a0824cbb66d5d1776
Dallas Ward <jetlock@gmail.com>
2020-04-02 23:13:13 -0400

    Removed unecessary lines

--- check-in/src/app/check-in-grid/check-in-grid.component.ts
+++ check-in/src/app/check-in-grid/check-in-grid.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Volunteer } from '../volunteer/volunteer';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CheckInPopupComponent } from '../check-in-popup/check-in-popup.component'
 
 @Component({
--- check-in/src/app/check-in-popup/check-in-popup.component.ts
+++ check-in/src/app/check-in-popup/check-in-popup.component.ts
@@ -1,19 +1,16 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-check-in-popup',
   templateUrl: './check-in-popup.component.html',
   styleUrls: ['./check-in-popup.component.css']
 })
-export class CheckInPopupComponent implements OnInit {
+export class CheckInPopupComponent {
 
   constructor(
     public dialogRef: MatDialogRef<CheckInPopupComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: 'Hi'
   ) { }
-
-  ngOnInit() {
-  }
 
 }
